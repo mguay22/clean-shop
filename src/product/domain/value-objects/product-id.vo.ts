dc6c0d0eb5d507fb25af4054b6f6f3617b393e26
Michael Guay <guay.me@gmail.com>
2026-03-05 17:44:17 -0500

    product entity starting props

--- src/product/domain/value-objects/product-id.vo.ts
+++ src/product/domain/value-objects/product-id.vo.ts
@@ -1,7 +1,7 @@
 import { UniqueId } from '../../../shared/domain/value-objects/unique-id.vo';
 
 export class ProductId extends UniqueId {
-  constructor(id: string) {
+  constructor(id?: string) {
     super(id);
   }
 }
