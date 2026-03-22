1e3292b8d3b5297ed4bdf7bb8a46523aacee33fc
Michael Guay <guay.me@gmail.com>
2026-03-22 16:00:15 -0400

    product controller + dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongoModule } from './shared/infrastructure/database/mongodb/mongo.module';
 import { DrizzleModule } from './shared/infrastructure/database/postgres/drizzle.module';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
+    CqrsModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
+    ProductModule,
     MongoModule,
     DrizzleModule,
   ],
