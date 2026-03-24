e90b8e5a19f0add1efc076e4a2e1c866867cb622
Michael Guay <guay.me@gmail.com>
2026-03-23 20:55:04 -0400

    create product handler

---
+++ src/product/application/use-cases/create-product/create-product.handler.ts
@@ -0,0 +1,28 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CreateProductCommand } from './create-product.command';
+import { Inject } from '@nestjs/common';
+import {
+  PRODUCT_REPOSITORY,
+  ProductRepository,
+} from '../../ports/product.repository.port';
+import { Product } from '../../../../product/domain/entities/product.entity';
+
+@CommandHandler(CreateProductCommand)
+export class CreateProductHandler implements ICommandHandler<CreateProductCommand> {
+  constructor(
+    @Inject(PRODUCT_REPOSITORY)
+    private readonly productRepository: ProductRepository,
+  ) {}
+
+  async execute(command: CreateProductCommand): Promise<void> {
+    const product = Product.create(
+      command.name,
+      command.description,
+      command.sku,
+      command.price,
+      command.currency,
+      command.stock,
+    );
+    await this.productRepository.save(product);
+  }
+}
--- src/product/presentation/product.controller.ts
+++ src/product/presentation/product.controller.ts
@@ -1,4 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { ProductResponseDto } from './dtos/product-response.dto';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateProductCommand } from '../application/use-cases/create-product/create-product.command';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly commandBus: CommandBus) {}
+
+  @Post()
+  async create(@Body() dto: CreateProductDto): Promise<void> {
+    await this.commandBus.execute(
+      new CreateProductCommand(
+        dto.name,
+        dto.description,
+        dto.sku,
+        dto.price,
+        dto.currency || 'USD',
+        dto.stock,
+      ),
+    );
+  }
+}
