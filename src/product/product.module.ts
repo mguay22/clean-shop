2437a23e94ded2622223ff480fc36f41f5342be7
Michael Guay <guay.me@gmail.com>
2026-04-09 17:27:06 -0400

    Mongo repo + env

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -5,6 +5,8 @@ import { PRODUCT_REPOSITORY } from './application/ports/product.repository.port'
 import { DrizzleProductRepository } from './infrastructure/adapters/drizzle-product.repository';
 import { CommandHandlers } from './application';
 import { QueryHandlers } from './application/queries/handlers';
+import { ConfigService } from '@nestjs/config';
+import { MongoProductRepository } from './infrastructure/adapters/mongo-product.repository';
 
 @Module({
   imports: [CqrsModule],
@@ -12,9 +14,20 @@ import { QueryHandlers } from './application/queries/handlers';
   providers: [
     ...CommandHandlers,
     ...QueryHandlers,
+    DrizzleProductRepository,
+    MongoProductRepository,
     {
       provide: PRODUCT_REPOSITORY,
-      useClass: DrizzleProductRepository,
+      useFactory: (
+        configService: ConfigService,
+        mongoRepo: MongoProductRepository,
+        drizzleRepo: DrizzleProductRepository,
+      ) => {
+        return configService.get('DATABASE') === 'mongodb'
+          ? mongoRepo
+          : drizzleRepo;
+      },
+      inject: [ConfigService, MongoProductRepository, DrizzleProductRepository],
     },
   ],
 })
