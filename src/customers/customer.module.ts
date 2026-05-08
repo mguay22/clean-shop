1d93edaa6affcbf5c98be2e4da6f878f61ab07a1
Michael Guay <guay.me@gmail.com>
2026-05-07 21:15:56 -0400

    Nodemailer adapter provider

--- src/customers/customer.module.ts
+++ src/customers/customer.module.ts
@@ -5,8 +5,8 @@ import { CommandHandlers } from './application/use-cases';
 import { CustomerController } from './presentation/customer.controller';
 import { QueryHandlers } from './application/queries/handlers';
 import { NOTIFICATION_SERVICE } from './application/ports/notification.port';
-import { ConsoleNotificationAdapter } from './infrastructure/adapters/console-notification.adapter';
 import { EventHandlers } from './application/events';
+import { NodemailerEmailAdapter } from './infrastructure/adapters/nodemailer-notification.adapter';
 
 @Module({
   controllers: [CustomerController],
@@ -20,7 +20,7 @@ import { EventHandlers } from './application/events';
     },
     {
       provide: NOTIFICATION_SERVICE,
-      useClass: ConsoleNotificationAdapter,
+      useClass: NodemailerEmailAdapter,
     },
   ],
 })
